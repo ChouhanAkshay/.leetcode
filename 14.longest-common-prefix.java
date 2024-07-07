871c3989b15b30cb311093d0bf76e0a38b3b615e
Akshay_byju <akshaychouhan.byjus@aesl.in>
2024-07-07 16:33:47 +0530

    sol

---
+++ 14.longest-common-prefix.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=14 lang=java
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String prefix = strs[0];
+        for (String s : strs)
+            while (s.indexOf(prefix) != 0)
+                prefix = prefix.substring(0, prefix.length() - 1);
+        return prefix;
+    }
+}
+// @lc code=end
+

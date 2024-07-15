031fe5df28b06f69c65c461dc175b498f161ec7c
Akshay_byju <akshaychouhan.byjus@aesl.in>
2024-07-15 15:16:23 +0530

    added

---
+++ 104.maximum-depth-of-binary-tree.java
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode id=104 lang=java
+ *
+ * [104] Maximum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        int count = 0;
+
+        if(root == null) {
+            return 0;
+        }
+
+        q.add(root);
+
+        while(!q.isEmpty()) {
+
+            int qSize = q.size();
+            count ++;
+
+            for(int i = 0 ; i < qSize ; i++) {
+                TreeNode node = q.remove();
+
+                if(node.left != null) {
+                    q.add(node.left);
+                }
+
+                if(node.right != null) {
+                    q.add(node.right);
+                }
+            }
+        }
+
+        return count;
+}
+// @lc code=end
+
